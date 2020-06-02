c0ab6c4170150421cbc81964b25b091f81a651f0
mariusflemmer <63319695+mariusflemmer@users.noreply.github.com>
2020-06-01 22:12:40 -0400

    Functional Components
    Converted some of project's components and methods to functional components. Also gained practice in using object destructuring syntax in function parameter lists.

--- CampsiteInfoComponent.js
+++ CampsiteInfoComponent.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class CampSiteInfo extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
 
-    renderCampsite(campsite) {
-            return (
-                <div className="col-md-5 m-1">
-                    <Card>
-                        <CardImg top src={campsite.image} alt={campsite.name} />
-                        <CardBody>
-                            <CardTitle>{campsite.name}</CardTitle>
-                            <CardText>{campsite.description}</CardText>
-                        </CardBody>
-                    </Card>
-                </div>
-            );
-        }
+function RenderCampsite({campsite}) {
+    return (
+        <div className="col-md-5 m-1">
+            <Card>
+                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardTitle>{campsite.name}</CardTitle>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
 
-    renderComments(comments) {
+function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -42,21 +35,18 @@ class CampSiteInfo extends Component {
             )
     }
 
-    render () {
-        if (this.props.campsite) {
+    function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
                     </div>
                 </div>
             );
         }
-        else {
-            return <div></div>
-        }
+        return <div />;
     }
-}
-
-export default CampSiteInfo;
+    
+    export default CampsiteInfo;
