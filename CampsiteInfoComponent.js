436cdb58a6951a36e0e8d85c3ebcd28b00c6ea1b
mariusflemmer <63319695+mariusflemmer@users.noreply.github.com>
2020-06-17 22:18:33 -0400

    Fetch Post Comment
    Learned how to use Fetch to send a POST request to a web server in order to add a newly submitted comment. You learned to handle the response from the POST request as well.

--- CampsiteInfoComponent.js
+++ CampsiteInfoComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     render () {
@@ -118,7 +118,7 @@ function RenderCampsite({campsite}) {
     );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -129,7 +129,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         -- {comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}
                         </p>
                     </div>)}
-                    <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                    <CommentForm campsiteId={campsiteId} postComment={postComment} />
                 </div>
             );
         }
@@ -176,7 +176,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                         <RenderCampsite campsite={props.campsite} />
                         <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                     </div>           
